0f4d02b4796820116bc896396051131a55e3e370
Beatriz Freitas <beatrizfreitasxbia@gmail.com>
2021-08-09 08:27:59 -0300

    Att

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -10,7 +10,7 @@ class Model {
 
     $host = $_ENV["database_host"];
     $user = $_ENV["database_user"];
-    $password = $_ENV["database_pass"]
+    $password = $_ENV["database_pass"];
 
         if(!isset(self::$conexao)){
             self::$conexao = new \PDO("mysql:host=$host;port=3306;dbname=fastparking;", $user, $password);
